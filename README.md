772b23a96ea45ebc050cb7b33d55266956bf5430
Gerard Costa <gerard.costa.mansilla@gmail.com>
2017-04-18 01:36:40 +0200

    Create bash script to import projects

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # import-git-repos
 Simple bash script to download and pack git repositories
+
+## How to use it
+Create a txt file with a list of projects you want to import
+
+Then call the script
+```bash
+./import-projects.sh projects.txt
+```
+
+This will clone all projects defined in `projects.txt` into projects directory 
+(in the current directory). If some of the repositories alredy exist, it will
+pull it instead of clone it. When finishes importing all projects, it will also
+create a projects.tar.gz file.
---
+++ import-projects.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+if [ ! $# -eq 1 ]; then
+  echo "You must specify a file with projects to import" >> /dev/stderr
+  exit 1
+fi
+
+file=$1
+regex="\/([^\.\/]+).git$"
+
+if [ ! -d "projects" ]; then
+  mkdir projects
+fi
+
+while IFS= read line
+do
+    if [[ $line =~ $regex ]]
+    then
+        name="${BASH_REMATCH[1]}"
+        if [ -d projects/$name ]; then
+          echo -e "\e[1;33m~ \e[1;39mPulling changes for \e[32m\"$name\"\e[0m ..."
+          cd projects/$name && git pull origin master -q && cd ../..
+        else
+           echo -e "\e[1;32m+ \e[1;39mImporting \e[32m\"$name\"\e[0m ..."
+           git clone $line projects/$name -q
+        fi
+    else
+        echo "$f doesn't match" >&2 
+    fi
+done <"$file"
+
+echo -e "\e[1mCompressing projects into \e[32mprojects.tar.gz\e[39m ...\e[0m"
+tar -czf projects.tar.gz projects
+size=`du -h projects.tar.gz | cut -f 1`
+echo -e "\e[1mCompressed size: \e[32m$size"
